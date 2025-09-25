2c053c3d2edbe5ec40f70dc4d16e7854eba8701a
guillot bertrand <guillotbertrand95@gmail.com>
2025-09-25 16:11:24 +0200

    fin de gestion manager

--- routes/staff.js
+++ routes/staff.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
-const roleMiddleware = require("../middlewares/roleMiddleware.js");
+const roleMiddleware = require("../middlewares/roleMiddleware");
 const userController = require("../controllers/userController");
 
-// Seul le manager peut créer un staff
+// ------------------- CREATE -------------------
+// Seul le manager peut créer un collaborateur
 router.post(
 	"/",
 	authMiddleware,
@@ -12,5 +13,31 @@ router.post(
 	userController.createStaff
 );
 
-// Tu peux ajouter GET, PUT, DELETE ici plus tard
+// ------------------- READ -------------------
+// Récupérer tous les collaborateurs
+router.get(
+	"/",
+	authMiddleware,
+	roleMiddleware(["manager"]),
+	userController.getStaff
+);
+
+// ------------------- UPDATE -------------------
+// Modifier un collaborateur existant
+router.put(
+	"/:id",
+	authMiddleware,
+	roleMiddleware(["manager"]),
+	userController.updateStaff
+);
+
+// ------------------- DELETE -------------------
+// Supprimer un collaborateur
+router.delete(
+	"/:id",
+	authMiddleware,
+	roleMiddleware(["manager"]),
+	userController.deleteStaff
+);
+
 module.exports = router;
